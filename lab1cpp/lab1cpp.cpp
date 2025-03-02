1b3bb3a49a9682a89eb5c4a2dbb26d0643ac75b8
MaxLis141B <lisovyi.maksym@chnu.edu.ua>
2025-03-02 21:25:43 +0200

    tasks

--- lab1cpp/lab1cpp.cpp
+++ lab1cpp/lab1cpp.cpp
@@ -4,50 +4,44 @@
 #include <io.h>
 #include <fcntl.h>
 #include <algorithm>
-
 using namespace std;
 
 template <typename T>
-void InputVector(vector<T>& vec, const wstring& message)
-{
+void InputVector(vector<T>& vec, const wstring& message) {
     wcout << message;
-    for (size_t i = 0; i < vec.size(); i++)
-    {
-        wcin >> vec[i];
+    for (auto& val : vec) {
+        wcin >> val;
     }
 }
 
 template <typename T>
-void PrintVector(const vector<T>& vec, const wstring& message)
-{
+void PrintVector(const vector<T>& vec, const wstring& message) {
     wcout << message << L": ";
-    for (const auto& val : vec)
-    {
+    for (const auto& val : vec) {
         wcout << val << L" ";
     }
     wcout << endl;
 }
 
-int main() {
-    _setmode(_fileno(stdout), _O_U8TEXT);
-    _setmode(_fileno(stdin), _O_U8TEXT);
-
+void Task1() {
     wcout << L"ЗАВДАННЯ 1" << endl;
-    wcout << L"Введіть розмір масиву: ";
     int N;
+    wcout << L"Введіть розмір масиву: ";
     wcin >> N;
 
     vector<int> A(N), C(N);
     InputVector(A, L"Введіть елементи масиву A: ");
 
-    for (int i = 0; i < N; i++)
-    {
+    for (int i = 0; i < N; ++i) {
         C[i] = pow(2, i) * A[i];
     }
 
     PrintVector(C, L"Результат (масив C)");
+}
 
-    wcout << endl << L"ЗАВДАННЯ 2" << endl;
+void Task2() {
+    wcout << L"ЗАВДАННЯ 2" << endl;
+    int N;
     wcout << L"Введіть розмір масиву: ";
     wcin >> N;
 
@@ -56,51 +50,37 @@ int main() {
 
     int c, d, max = INT_MIN;
     bool found = false;
+
     wcout << L"Введіть значення c: ";
     wcin >> c;
     wcout << L"Введіть значення d: ";
     wcin >> d;
 
-    for (int i = 0; i < N; i++)
-    {
-        if (B[i] >= c && B[i] <= d)
-        {
-            if (B[i] > max)
-            {
+    for (int i = 0; i < N; ++i) {
+        if (B[i] >= c && B[i] <= d) {
+            if (B[i] > max) {
                 max = B[i];
                 found = true;
             }
-            if (i < N - 1 && B[i + 1] % 2 == 0)
-            {
-                break;
-            }
+            if (i < N - 1 && B[i + 1] % 2 == 0) break;
         }
     }
 
-
-    if (!found)
-    {
+    if (!found) {
         wcout << L"Значення c і d поза межами масиву" << endl;
     }
-    else
-    {
+    else {
         wcout << L"Результат (максимальний елемент): " << max << endl;
     }
+}
 
-    wcout << endl << L"ЗАВДАННЯ 3" << endl;
-    wcout << L"Введіть розмір масиву (≤ 500): ";
-    do
-    {
+void Task3() {
+    wcout << L"ЗАВДАННЯ 3" << endl;
+    int N;
+    do {
+        wcout << L"Введіть розмір масиву (≤ 500): ";
         wcin >> N;
-        if (N > 500)
-        {
-            wcout << L"Розмір масиву не має перевищувати 500. Введіть ще раз: ";
-        }
-        else
-        {
-            break;
-        }
-    } while (true);
+    } while (N > 500);
 
     vector<float> A1(N);
     InputVector(A1, L"Введіть елементи масиву A: ");
@@ -110,36 +90,43 @@ int main() {
     float sum = 0, mult = 1;
     bool newnum = true;
 
-    for (int i = 0; i < N; i++)
-    {
-        if (i < N - 1 && A1[i] == A1[i + 1])
-        {
+    for (int i = 0; i < N; ++i) {
+        if (i < N - 1 && A1[i] == A1[i + 1]) {
             sum += A1[i];
             newnum = false;
         }
-        if (i < N - 1 && A1[i] != A1[i + 1] && !newnum)
-        {
-            sum += A1[i];
-            newnum = true;
-        }
-        if (i < N - 1 && A1[i] != A1[i + 1])
-        {
-            mult *= A1[i];
-            newnum = true;
+        else {
+            if (!newnum) {
+                sum += A1[i];
+                newnum = true;
+            }
+            else {
+                mult *= A1[i];
+            }
         }
     }
 
-    if (newnum)
-    {
-        mult *= A1[N - 1];
-    }
-    else
-    {
-        sum += A1[N - 1];
-    }
+    wcout << L"Результат (сума повторень): " << sum << endl;
+    wcout << L"Результат (добуток унікальних): " << mult << endl;
+}
 
-    wcout << L"Результат (сума): " << sum << endl;
-    wcout << L"Результат (добуток): " << mult << endl;
+int main() {
+    _setmode(_fileno(stdout), _O_U8TEXT);
+    _setmode(_fileno(stdin), _O_U8TEXT);
+
+    int choice;
+    do {
+        wcout << L"\nВиберіть завдання (1-3) або 0 для виходу: ";
+        wcin >> choice;
+
+        switch (choice) {
+        case 1: Task1(); break;
+        case 2: Task2(); break;
+        case 3: Task3(); break;
+        case 0: break;
+        default: wcout << L"Невірний вибір!" << endl;
+        }
+    } while (choice != 0);
 
     return 0;
 }
